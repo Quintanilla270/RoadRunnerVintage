0d667ad9eeb2c9df8ec42022090ed28e127a2977
Savior001 <45613403+Savior001@users.noreply.github.com>
2023-11-26 18:31:29 -0600

    updating login_handler.php

--- php/login_handler.php
+++ php/login_handler.php
@@ -4,28 +4,33 @@ require_once 'rrv_database.php';
 header('Content-Type: application/json');
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = mysqli_real_escape_string($conn, $_POST['username']);
-    $password = mysqli_real_escape_string($conn, $_POST['password']);
+    if (isset($_POST['username']) && isset($_POST['password'])) {
+        $username = mysqli_real_escape_string($conn, $_POST['username']);
+        $password = mysqli_real_escape_string($conn, $_POST['password']);
 
-    $query = "SELECT * FROM users WHERE username = '$username'";
-    $result = $conn->query($query);
-
-    if ($result->num_rows == 1) {
-        $row = $result->fetch_assoc();
-        $hashedPassword = $row['password'];
-
-        if (password_verify($password, $hashedPassword)) {
-            $_SESSION['username'] = $username;
-            echo json_encode(['success' => true, 'message' => 'Login successful']);
+        $stmt = $conn->prepare("SELECT * FROM users WHERE username = '$username'");
+        $stmt->bind_param("s", $_POST['username']);
+        $stmt->execute();
+        $result = $stmt->get_result();
+    
+        if ($result->num_rows == 1) {
+            $row = $result->fetch_assoc();
+            $hashedPassword = $row['password'];
+    
+            if (password_verify($password, $hashedPassword)) {
+                $_SESSION['username'] = $username;
+                echo json_encode(['success' => true, 'message' => 'Login successful']);
+            } else {
+                echo json_encode(['success' => false, 'message' => 'Invalid username or password']);
+            }
         } else {
             echo json_encode(['success' => false, 'message' => 'Invalid username or password']);
         }
+        $stmt->close();
     } else {
         echo json_encode(['success' => false, 'message' => 'Invalid username or password']);
     }
 } else {
     echo json_encode(['success' => false, 'message' => 'Invalid request']);
 }
-
-$conn->close();
 ?>
