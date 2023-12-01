3ad56074cedb2968d4da65fdb9697506b9823b6c
Quintanilla270 <30916119+Quintanilla270@users.noreply.github.com>
2023-11-30 19:52:32 -0600

    cook

--- php/index.php
+++ php/index.php
@@ -7,14 +7,18 @@ $cookie_value = "true";
 // Set the cookie expiration time to 30 days
 $expiration_time = time() + (60 * 60 * 24 * 30);
 
-// Set the cookie
-setcookie($cookie_name, $cookie_value, $expiration_time);
+if (!isset($_COOKIE[$cookie_name])) {
+    // Set the cookie
+    setcookie($cookie_name, $cookie_value, $expiration_time);
 
-// Check if the cookie is set
-if(isset($_COOKIE[$cookie_name])) {
-    alert("Cookie '" . $cookie_name . "' is set.");
-} else {
-    alert("Cookie '" . $cookie_name . "' is not set.");
+    // Tell the user that the cookie has been set
+    echo "The cookie has been set.";
+} 
+else {
+    // Tell the user that the cookie is already set
+    echo "The cookie is already set.";
 }
+
+?>
 
 ?>
